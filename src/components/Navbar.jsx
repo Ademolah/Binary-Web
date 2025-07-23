aa84f1130e8c129d17c5db03912dda4683970c44
Charles <adeakinyemi129@gmail.com>
2025-07-23 19:51:26 +0100

    adjusted about us

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   const navLinks = [
     { name: 'Home', path: '/' },
     { name: 'Services', path: '/services' },
-    { name: 'About', path: '/about' },
+    { name: 'About Us', path: '/about' },
     { name: 'Contact', path: '/contact' },
   ];
 
